dfb31bdb614a68c8d0790519854163e6cfbf2cd6
clpowe <clpowe@gmail.com>
2024-11-13 20:47:42 -0500

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,35 @@ import svgLoader from 'vite-svg-loader'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+<<<<<<< HEAD
+=======
+	compatibilityDate: '2024-10-04',
+	future: {
+		compatibilityVersion: 4
+	},
+>>>>>>> 6dd3464 (update config)
 	app: {
 		head: {
 			htmlAttrs: {
 				lang: 'en-US'
 			},
 			charset: 'utf-16',
+<<<<<<< HEAD
 			viewport: 'width=500, initial-scale=1',
 			title: 'Focus Flooring',
 			meta: [
 				// <meta name="description" content="My amazing site">
 				{ name: 'description', content: 'My amazing site.' }
+=======
+			viewport: 'width=device-width,initial-scale=1.0',
+			title: 'Focus Flooring',
+			meta: [
+				{
+					name: 'description',
+					content:
+						'Looking for reliable commercial flooring solutions? Focus Flooring in Tampa delivers projects on time and within budget. Our minority-owned business offers expert installation of carpet, wood, tile, stone, and more, ensuring precision, cost certainty. Contact us for top-quality, innovative flooring in healthcare, industrial, aviation, and other sectors.'
+				}
+>>>>>>> 6dd3464 (update config)
 			],
 			link: [
 				{
@@ -30,6 +48,7 @@ export default defineNuxtConfig({
 			]
 		}
 	},
+<<<<<<< HEAD
 	css: ['@/assets/css/tailwind.scss'],
 	vite: {
 		plugins: [
@@ -52,10 +71,56 @@ export default defineNuxtConfig({
 		'@nuxt/ui',
 		'@nuxtjs/google-fonts',
 		'nuxt-icon',
+=======
+
+	routeRules: {
+		'/': { prerender: true },
+		'/about-us': { prerender: true },
+		'join-our-team': { prerender: true },
+		'our-history': { prerender: true },
+		'our-process': { prerender: true }
+	},
+
+	css: [
+		'@/assets/css/tokens.css',
+		'@/assets/css/reset.css',
+		'@/assets/css/tailwind.css'
+	],
+
+	vite: {
+		css: {
+			lightningcss: {}
+		},
+		plugins: [
+			svgLoader() // https://github.com/jpkleemans/vite-svg-loader#readme
+		]
+	},
+
+	devtools: {
+		componentInspector: true,
+		enabled: true
+	},
+
+	sourcemap: {
+		server: true,
+		client: false
+	},
+
+	extends: ['@nuxt/ui-pro'],
+
+	modules: [
+		'nuxt-vercel-analytics',
+		'@vueuse/nuxt',
+		'@nuxt/ui',
+		'@formkit/auto-animate/nuxt',
+		'@nuxtjs/google-fonts',
+		'@nuxt/icon',
+>>>>>>> 6dd3464 (update config)
 		'@nuxtjs/robots',
 		'@nuxt/image-edge',
 		'@nuxtjs/tailwindcss',
 		'nuxt-schema-org',
+<<<<<<< HEAD
 		'nuxt-swiper',
 		'@nuxt/image'
 	],
@@ -68,13 +133,68 @@ export default defineNuxtConfig({
 	},
 	plugins: ['~/plugins/preline.client.ts'],
 	googleFonts: {
+=======
+		'@nuxt/image',
+		'@nuxt/scripts',
+		'@nuxtjs/sitemap'
+	],
+
+	nitro: {
+		compressPublicAssets: true,
+		devProxy: {
+			host: '127.0.0.1'
+		},
+		routeRules: {
+			'/team': {
+				cache: {
+					maxAge: 20 * 60 * 1000,
+					swr: true
+				}
+			},
+			'/projects': {
+				cache: {
+					maxAge: 20 * 60 * 1000,
+					swr: true
+				}
+			}
+		}
+	},
+
+	features: {
+		devLogs: true
+	},
+
+	colorMode: {
+		preference: 'light'
+	},
+
+	googleFonts: {
+		display: 'swap',
+>>>>>>> 6dd3464 (update config)
 		families: {
 			Inter: [400, 700]
 		}
 	},
+<<<<<<< HEAD
 	build: {
 		transpile: ['@nuxt/image']
 	},
+=======
+	$production: {
+		scripts: {
+			registry: {
+				// googleAnalytics: {
+				// 	id: 'G-43HSQWN2L1'
+				// }
+			}
+		}
+	},
+
+	build: {
+		transpile: ['@nuxt/image']
+	},
+
+>>>>>>> 6dd3464 (update config)
 	postcss: {
 		plugins: {
 			'postcss-preset-env': {
@@ -82,18 +202,28 @@ export default defineNuxtConfig({
 				features: {
 					'nesting-rules': true
 				}
+<<<<<<< HEAD
 			}
 		}
 	},
 	formkit: {
 		configFile: './formkit.config.ts'
 	},
+=======
+			},
+			cssnano: {},
+			autoprefixer: {}
+		}
+	},
+
+>>>>>>> 6dd3464 (update config)
 	image: {
 		format: ['avif', 'webp'],
 		imagekit: {
 			baseURL: 'https://ik.imagekit.io/focusflooring/'
 		}
 	},
+<<<<<<< HEAD
 	schemaOrg: {
 		host: 'https://focus-flooring.com'
 	},
@@ -110,4 +240,15 @@ export default defineNuxtConfig({
 			}
 		}
 	}
+=======
+
+	schemaOrg: {
+		host: 'https://focus-flooring.com'
+	},
+	site: {
+		url: 'https://focus-flooring.com',
+		name: 'Focus Flooring'
+	},
+	sitemap: {}
+>>>>>>> 6dd3464 (update config)
 })
